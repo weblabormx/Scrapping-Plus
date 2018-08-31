a0327cbe2037d487f1d062b41d98f025a43386aa
Carlos Escobar <skalero01@gmail.com>
2018-08-31 16:32:08 -0500

    add voku driver

---
+++ src/Drivers/Voku.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace WeblaborMX\ScrappingPlus\Drivers;
+
+use WeblaborMX\ScrappingPlus\DriverFormat;
+use voku\helper\HtmlDomParser;
+use Illuminate\Support\Collection;
+
+class Voku extends DriverFormat
+{
+
+    public $object;
+    public $selector;
+
+    public function setUrl($url) 
+    {
+        $opts = array(
+            'http'=>array(
+                'method'=>"GET",
+                'header'=>"Accept-language: en\r\n" .
+                "User-Agent:    Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6\r\n".
+                "Cookie: foo=bar\r\n"
+            )
+        );
+        $context = stream_context_create($opts);
+        $html = file_get_contents($url, false, $context);
+        $this->setHtml($html);
+        return $this;
+    }
+
+    public function setHtml($html) {
+        $dom = HtmlDomParser::str_get_html($html);
+        $this->object = $dom;
+        return $this;
+    }
+
+    // Getters 
+
+    public function get($selector) 
+    {
+        return collect($this->object->find($selector))->map(function($item) {
+            $object = new Voku;
+            $object->selector = $item;
+            return $object->setHtml($item->outerHtml);
+        });
+    }
+
+    // Attributes
+
+    public function getHtml() 
+    {
+        return $this->object->outertext;
+    }
+
+    public function getAttribute($name) 
+    {
+        if(!isset($this->selector)) {
+            return;
+        }
+        return $this->selector->$name;
+    }
+
+    public function getLink() 
+    {
+        return $this->getAttribute('href');
+    }
+
+    public function getText() 
+    {
+        return $this->selector->plaintext;
+    }
+    
+}
--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -83,4 +83,48 @@ class BasicTest extends TestCase
         $this->assertEquals('Me siento con suerte ', $title);
     }
 
+    /** @test */
+    public function vokuTest()
+    {
+        $google = Scrapping::method('voku')->scrappe('https://www.google.com.mx');
+        $html = $google->getHtml();
+
+        // Access inputs
+        $inputs = $google->get('input');
+
+        $first = $inputs->first();
+        $second = $inputs[1];
+
+        $class = $google->first('input[name=btnI]');
+        $title = $class->getAttribute('value');
+
+        $this->assertEquals('Me siento con suerte ', $title);
+    }
+
+    /** @test */
+    public function bug1()
+    {
+        $page = Scrapping::method('voku')->scrappe('https://www.eticket.mx/masinformacion.aspx?idevento=23878');
+        $image_header_url = $page->first('#copetes_dinamicos .ancholimitado img')->getAttribute('src');
+        $image_poster_url = $page->first('.campo2_hor_izq .font14 img')->getAttribute('src');
+        $image_map_url    = $page->first('#mapwrapper img')->getAttribute('src');
+        $title            = $page->first('#copetes_dinamicos .ancholimitado img')->getAttribute('alt');
+        $date             = $page->first('.grisclarofondo .mayusculas_primera')->getText();
+        $hour             = $page->first('.grisclarofondo .mayusculas_primera > span')->getText();
+        $hour             = str_replace('(', '', $hour);
+        $hour             = str_replace(')', '', $hour);
+        $address_object = $page->get('.grisclarofondo > div')[2];
+        $address_object = $address_object->get('div > div');
+        $place       = $address_object[0]->getText();
+        $this->assertTrue(!is_null($place));
+        $city        = $address_object[1]->getText();
+        $this->assertTrue(!is_null($city));
+        $address     = $address_object[2]->getText();
+        $this->assertTrue(!is_null($address));
+        $neighbor    = $address_object[3]->getText();
+        $this->assertTrue(!is_null($neighbor));
+        $postal_code = $address_object[4]->getText();
+        $this->assertTrue(!is_null($postal_code));
+    }
+
 }
